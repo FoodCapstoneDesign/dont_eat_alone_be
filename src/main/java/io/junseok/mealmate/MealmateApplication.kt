49f54c1bbb8e94ffc4ea593bb1c61bbca236ca47
JunRock <wnstjr120422@naver.com>
2024-06-30 01:04:13 +0900

    fix : main class change by kotlin

--- src/main/java/io/junseok/mealmate/MealmateApplication.kt
+++ src/main/java/io/junseok/mealmate/MealmateApplication.kt
@@ -1,13 +1,13 @@
 package io.junseok.mealmate
 
-import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
 @EnableJpaAuditing
-class MealmateApplication {
+class MealmateApplication
     fun main(args: Array<String>) {
-        SpringApplication.run(MealmateApplication::class.java, *args)
+        runApplication<MealmateApplication>(*args)
     }
-}
+
